24984708774f50675782907a9cb72eef7c596f4f
Dixit Bafna <Dixit.Bafna@ing.com>
2020-08-02 10:17:22 +0200

    Proxy config for deployment

--- security/src/store/action/tracks.js
+++ security/src/store/action/tracks.js
@@ -15,7 +15,7 @@ export const fetchTracksFailed = (code) => {
 }
 export const fetchTracks = () => {
     return (dispatch) => {
-        fetch(`https://mis.ssreng.com/api/gtctracks?page=1&size=2000&updateSince=2020-07-01T17:15:00Z&expand=detail`, {
+        fetch(`/api/gtctracks?page=1&size=2000&updateSince=2020-07-01T17:15:00Z&expand=detail`, {
             headers: {
                 Authorization: 'Basic X2RlbW9fOl9kZW1vXzAw',
             }
